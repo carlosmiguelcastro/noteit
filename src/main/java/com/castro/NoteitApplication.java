e13ccd0062715957a378e8ee0b91db20ce3cd304
Carlos Castro <flabron@gmail.com>
2017-01-26 00:08:26 +0000

    Initial commit

---
+++ src/main/java/com/castro/NoteitApplication.java
@@ -0,0 +1,12 @@
+package com.castro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteitApplication.class, args);
+	}
+}
